c1deda1f351e8d382edd97ea2c49be8ad8f7938f
Yangke <liyangke2014@gmail.com>
2022-03-19 06:02:43 +0000

    rate css finish

--- gamehub/forms.py
+++ gamehub/forms.py
@@ -23,7 +23,7 @@ class CommentForm(forms.ModelForm):
 
     
     #game = models.Game()
-    id = forms.IntegerField(widget=forms.NumberInput)
+    #id = forms.IntegerField(widget=forms.NumberInput)
     content = forms.CharField(max_length=300)
     quality_rate = forms.IntegerField(widget=forms.NumberInput,max_value=5, initial=0)
     music_rate = forms.IntegerField(widget=forms.NumberInput,max_value=5, initial=0)
@@ -32,4 +32,14 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         #exclude = ('game',)
-        fields = ('id','content','quality_rate','music_rate','community_rate',)
+        fields = ('content','quality_rate','music_rate','community_rate',)
+        
+
+    
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        #self.fields['music_rate'].widget.attrs['size'] = 20
+        self.fields['music_rate'].widget.attrs['class'] = 'number-input'
+        self.fields['quality_rate'].widget.attrs['class'] = 'number-input'
+        self.fields['community_rate'].widget.attrs['class'] = 'number-input'
+        self.fields['content'].widget.attrs['class'] = 'txt-input'
